6d708c355a96b6ad05741099afdb31a3012ef245
klimga <136005848+klimga@users.noreply.github.com>
2024-03-29 17:49:09 -0500

    Add files via upload

--- src/Components/indexP.ts
+++ src/Components/indexP.ts
@@ -1,5 +1,5 @@
+export { default as user } from './user/user';
 export { default as feed } from './feed/feed';
 export { default as followers } from './Followers/Followers';
 export { default as FolloBackgrond } from './FolloBackgroud/FolloBackgrond';
-export { default as trends } from './trends/trend';
-export { default as trending } from './trending/trending';
+export { default as NavApp } from './nav-app/nav-app';
--- src/indexA.ts
+++ src/indexA.ts
@@ -1,37 +1,21 @@
 import { UserData } from './data/data';
-import { trendData } from './data/trendsData';
 import './Components/indexP';
 import MyUser, { Attribute } from './Components/user/user';
 import MyFeed, { Attribute2 } from './Components/feed/feed';
 import Myfollower, { Attribute3 } from './Components/Followers/Followers';
-import MyTrend, { Attribute4 } from './Components/trends/trend';
-
 import stylesA from './styleA.css';
+
 class AppContainer extends HTMLElement {
 	users: MyUser[] = [];
-	feeds: MyFeed[] = [];
-	followers: Myfollower[] = [];
-	trends: MyTrend[] = [];
-	currentTrendIndex: number = 0;
-	currentTrendIndex2: number = 1;
-	currentTrendIndex3: number = 2;
-	currentTrendIndex4: number = 4;
+	feeds: MyFeed[]=[];
+	followers: Myfollower[]=[];
 	currentUserIndex: number = 0;
 	currentUserIndex2: number = 1;
 	currentUserIndex3: number = 2;
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
-	}
-
-	connectedCallback() {
-		trendData.forEach((trend) => {
-			const trendCard = this.ownerDocument.createElement('custom-trend') as MyTrend;
-			trendCard.setAttribute(Attribute4.trend, trend.trend);
-			trendCard.setAttribute(Attribute4.post, String(trend.post));
-			this.trends.push(trendCard);
-		});
-
+	
 		UserData.forEach((user) => {
 			const UserCard = this.ownerDocument.createElement('custom-user') as MyUser;
 			UserCard.setAttribute(Attribute.name, user.name);
@@ -42,15 +26,19 @@ class AppContainer extends HTMLElement {
 			this.users.push(UserCard);
 		});
 
-		UserData.forEach((feed) => {
+	}
+	connectedCallback() {
+
+	
+
+		UserData.forEach((feed)=> {
 			const FeedCard = this.ownerDocument.createElement('custom-feed') as MyFeed;
 			FeedCard.setAttribute(Attribute2.image, feed.image);
 			FeedCard.setAttribute(Attribute2.username, feed.username);
 			FeedCard.setAttribute(Attribute2.postimage, feed.postimage);
 			this.feeds.push(FeedCard);
 		});
-
-		UserData.forEach((follower) => {
+		UserData.forEach((follower)=> {
 			const followerCard = this.ownerDocument.createElement('custom-followers') as Myfollower;
 			followerCard.setAttribute(Attribute3.image, follower.image);
 			followerCard.setAttribute(Attribute3.username, follower.username);
@@ -58,32 +46,22 @@ class AppContainer extends HTMLElement {
 			this.followers.push(followerCard);
 		});
 
-		trendData.forEach((trend) => {
-			const trendCard = this.ownerDocument.createElement('custom-trend') as MyTrend;
-			trendCard.setAttribute(Attribute4.trend, trend.trend);
-			trendCard.setAttribute(Attribute4.post, String(trend.post));
-			this.trends.push(trendCard);
-		});
+		
 
 		this.render();
 	}
-
 	render() {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
-
+			
 				<style>
 					${stylesA}
 				</style>
 				<custom-follobackgrond></custom-follobackgrond>
-				<custom-trending></custom-trending>
-
+				<nav-app></nav-app>
 			`;
 
-			const ContenedorUser = this.ownerDocument.createElement('div');
-			ContenedorUser.className = 'contenedoruser';
-			ContenedorUser.appendChild(this.users[this.currentUserIndex]);
-			this.shadowRoot?.appendChild(ContenedorUser);
+			this.shadowRoot.appendChild(this.users[this.currentUserIndex]);
 
 			const ContenedorFeed = this.ownerDocument.createElement('section');
 			ContenedorFeed.className = 'contenedorfeed';
@@ -92,6 +70,7 @@ class AppContainer extends HTMLElement {
 			});
 			this.shadowRoot?.appendChild(ContenedorFeed);
 
+			
 			const ContenedorFollowers = this.ownerDocument.createElement('div');
 			ContenedorFollowers.className = 'contenedorfollowers';
 			ContenedorFollowers.appendChild(this.followers[this.currentUserIndex]);
@@ -99,13 +78,6 @@ class AppContainer extends HTMLElement {
 			ContenedorFollowers.appendChild(this.followers[this.currentUserIndex3]);
 			this.shadowRoot?.appendChild(ContenedorFollowers);
 
-			const ContenedorTrends = this.ownerDocument.createElement('div');
-			ContenedorTrends.className = 'contenedortrend';
-			ContenedorTrends.appendChild(this.trends[this.currentTrendIndex]);
-			ContenedorTrends.appendChild(this.trends[this.currentTrendIndex2]);
-			ContenedorTrends.appendChild(this.trends[this.currentTrendIndex3]);
-			ContenedorTrends.appendChild(this.trends[this.currentTrendIndex4]);
-			this.shadowRoot?.appendChild(ContenedorTrends);
 		}
 	}
 }
