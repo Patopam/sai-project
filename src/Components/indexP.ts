bfcc33537e5a412d39e7d11f7cc3cfb70b424c4c
klimga <136005848+klimga@users.noreply.github.com>
2024-04-30 00:05:22 -0500

    Add files via upload

--- src/Components/indexP.ts
+++ src/Components/indexP.ts
@@ -1,13 +1,15 @@
-export { default as user } from './user/user';
-export { default as feed } from './feed/feed';
-export { default as followers } from './Followers/Followers';
-export { default as FolloBackgrond } from './FolloBackgroud/FolloBackgrond';
-export { default as NavApp } from './nav-app/nav-app';
-export { default as trends } from './trends/trend';
-export { default as trending } from './trending/trending';
-export { default as welcome } from './welcome/welcome';
-export { default as PhotoButton } from './photoButton/photoButton';
-export { default as VideoButton } from './videoButton/videoButton';
-export { default as folloButton } from './folloButton/folloButton';
-export { default as head } from './head/head';
-export { default as likeButton } from './likeButton/likeButton';
+export { default as user } from './Home/user/user';
+export { default as feed } from './Home/Folder feed/feed/feed';
+export { default as followers } from './Home/Folder Followers/Followers/Followers';
+export { default as FolloBackgrond } from './Home/Folder Followers/FolloBackgroud/FolloBackgrond';
+export { default as NavApp } from './Home/nav-app/nav-app';
+export { default as trends } from './Home/Folder trends/trends/trend';
+export { default as trending } from './Home/Folder trends/trending/trending';
+export { default as welcome } from './Home/Folder Welcome/welcome/welcome';
+export { default as PhotoButton } from './Home/Folder Welcome/photoButton/photoButton';
+export { default as VideoButton } from './Home/Folder Welcome/videoButton/videoButton';
+
+export { default as folloButton } from './Home/Folder Followers/folloButton/folloButton';
+export { default as head } from './Home/head/head';
+export { default as likeButton } from './Home/Folder feed/likeButton/likeButton';
+
