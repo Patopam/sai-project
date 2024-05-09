8354b7a545093036ac000c6f1f8eb6b168eb5104
Mar <m.d.ramirez.1404@gmail.com>
2024-05-09 02:23:21 -0500

    no

--- src/IndexA.ts
+++ src/IndexA.ts
@@ -6,39 +6,36 @@ import { appState } from './store/store';
 import { addObserver } from './store/store';
 
 class AppContainer extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+		addObserver(this);
+	}
 
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        addObserver(this);
-    }
-    
-    connectedCallback(){
-        this.render()
-    }
-    render(){
-        if(this.shadowRoot) this.shadowRoot.innerHTML = '';
-            switch (appState.screen) {
-                case 'SignIn':
-                    const SignIn = this.ownerDocument.createElement('app-signin')
-                    this.shadowRoot?.appendChild(SignIn)
-                    break;
+	connectedCallback() {
+		this.render();
+	}
+	render() {
+		if (this.shadowRoot) this.shadowRoot.innerHTML = '';
+		switch (appState.screen) {
+			case 'SignIn':
+				const SignIn = this.ownerDocument.createElement('app-signin');
+				this.shadowRoot?.appendChild(SignIn);
+				break;
 
-                    case 'Home':
-                        const Home = this.ownerDocument.createElement('app-home')
-                        this.shadowRoot?.appendChild(Home)
-                        break;
+			case 'Home':
+				const Home = this.ownerDocument.createElement('app-home');
+				this.shadowRoot?.appendChild(Home);
+				break;
 
-                        case 'LogIn':
-                            const LogIn = this.ownerDocument.createElement('app-login')
-                            this.shadowRoot?.appendChild(LogIn)
-                            break;
-                default:
-                    break;
-            }
-      
-        }
-    
-    }
-    
-    customElements.define('app-container', AppContainer)
+			case 'LogIn':
+				const LogIn = this.ownerDocument.createElement('app-login');
+				this.shadowRoot?.appendChild(LogIn);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+customElements.define('app-container', AppContainer);
