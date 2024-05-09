3e55a25cdd5d1563eab898e359d619756ea6c319
Patopam <145809595+Patopam@users.noreply.github.com>
2024-05-09 02:07:01 -0500

    Add files via upload

---
+++ src/store/store.ts
@@ -0,0 +1,17 @@
+import { reducer } from "./reducer";
+export let appState = {
+    screen:'SignIn',
+};
+
+
+let observers: any = [];
+
+export const dispatch = (action: any) => {
+	const clone = JSON.parse(JSON.stringify(appState));
+	appState = reducer(action, clone);
+	observers.forEach((o: any) => o.render());
+};
+
+export const addObserver = (ref: any) => {
+	observers = [...observers, ref];
+};
