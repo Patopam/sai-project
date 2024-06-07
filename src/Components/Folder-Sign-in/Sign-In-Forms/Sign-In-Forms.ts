ed1235239864a4593f09ca70f83ce879b23e4171
Patopam <145809595+Patopam@users.noreply.github.com>
2024-06-06 21:55:48 -0500

    Add files via upload

--- src/Components/Folder-Sign-in/Sign-In-Forms/Sign-In-Forms.ts
+++ src/Components/Folder-Sign-in/Sign-In-Forms/Sign-In-Forms.ts
@@ -1,7 +1,6 @@
 import Styles from './Sign-In-Forms.css';
 import { users } from '../../../types/users-sign';
 import { createUser } from '../../../services/firebase';
-import { appState } from '../../../store/store';
 
 const formData: Omit<users, 'id'> = {
 	name: '',
@@ -19,50 +18,51 @@ class Forms extends HTMLElement {
 
 	connectedCallback() {
 		this.render();
+		const changeName = this.shadowRoot?.querySelector('#changeForms1');
+		changeName?.addEventListener('change', this.changeName.bind(this));
+
+		const changeUsername = this.shadowRoot?.querySelector('#changeForms2');
+		changeUsername?.addEventListener('change', this.changeUsername.bind(this));
+
+		const changeEmail = this.shadowRoot?.querySelector('#changeForms3');
+		changeEmail?.addEventListener('change', this.changeEmail.bind(this));
+
+		const changePassword = this.shadowRoot?.querySelector('#changeForms4');
+		changePassword?.addEventListener('change', this.changePassword.bind(this));
+
+		const changeConfirmPass = this.shadowRoot?.querySelector('#changeForms5');
+		changeConfirmPass?.addEventListener('change', this.changeConfirmPass.bind(this));
+
 		const save = this.shadowRoot?.querySelector('custom-singbutton');
 		save?.addEventListener('click', this.submitForm.bind(this));
 	}
 
+	changeName(e: any) {
+		formData.name = e?.target?.value;
+	}
+
+	changeUsername(e: any) {
+		formData.username = e?.target?.value;
+	}
+
+	changeEmail(e: any) {
+		formData.email = e?.target?.value;
+	}
+
+	changePassword(e: any) {
+		formData.password = e?.target?.value;
+	}
+
+	changeConfirmPass(e: any) {
+		formData.confirmPass = e?.target?.value;
+	}
+
 	async submitForm() {
-		// Obtener el elemento errorMessage dentro de la función submitForm
-		const errorMessageElement = this.shadowRoot?.getElementById('errorMessage');
-		if (!errorMessageElement) {
-			console.error('Error: errorMessage element not found');
-			return;
-		}
-
-		errorMessageElement.innerHTML = '';
-		errorMessageElement.style.display = 'none';
-
-		// Validación de campos
-		if (!formData.name || !formData.username || !formData.email || !formData.password || !formData.confirmPass) {
-			errorMessageElement.innerHTML = 'All fields are required';
-			errorMessageElement.style.display = 'block';
-			return;
-		}
-
-		if (formData.password !== formData.confirmPass) {
-			errorMessageElement.innerHTML = 'Passwords do not match';
-			errorMessageElement.style.display = 'block';
-			return;
-		}
-
-		if (!isValidEmail(formData.email)) {
-			errorMessageElement.innerHTML = 'Invalid email';
-			errorMessageElement.style.display = 'block';
-			return;
-		}
-
-		// Crear usuario si los campos son válidos
 		try {
 			await createUser(formData.email, formData.password, formData.name, formData.username, formData.confirmPass);
 			console.log('User registered successfully');
-			// Cambiar la pantalla a Home solo si la creación de usuario fue exitosa
-			appState.screen = 'Home';
 		} catch (error) {
 			console.error('Error registering user: ', error);
-			errorMessageElement.innerHTML = 'Error registering user';
-			errorMessageElement.style.display = 'block';
 		}
 	}
 
@@ -75,29 +75,22 @@ class Forms extends HTMLElement {
 			this.shadowRoot?.appendChild(css);
 
 			this.shadowRoot.innerHTML = `
-        <style> ${Styles}</style>
+			<style> ${Styles}</style>
 
-        <section>
-          <div class="box-Forms">
-            <input id="changeForms1" class="Forms" type="text" name="Name" placeholder="Name">
-            <input id="changeForms2" class="Forms" type="text" name="Username" placeholder="Username">
-            <input id="changeForms3" class="Forms" type="email" name="Email" placeholder="Email">
-            <input id="changeForms4" class="Forms" type="password" name="Password" placeholder="Password">
-            <input id="changeForms5" class="Forms" type="password" name="ConfirmPassword" placeholder="Confirm Password">
-            <div id="errorMessage" style="color: red; display: none;"></div>
-          </div>
-          <custom-singbutton></custom-singbutton>
-        </section>
-      `;
+			<section>
+			<div class="box-Forms">
+				<input id="changeForms1" class="Forms" type="text" name="Name" placeholder="Name">
+                <input id="changeForms2" class="Forms" type="text" name="Username" placeholder="Username">
+                <input id="changeForms3" class="Forms" type="email" name="Email" placeholder="Email">
+                <input id="changeForms4" class="Forms" type="password" name="Password" placeholder="Password">
+                <input id="changeForms5" class="Forms" type="password" name="ConfirmPassword" placeholder="Confirm Password">
+			</div>
+				<custom-singbutton></custom-singbutton>
+			</section>
+			`;
 		}
 	}
 }
 
-// Función de validación de email
-function isValidEmail(email: string): boolean {
-	// Expresión regular para validar el formato del email
-	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	return emailRegex.test(email);
-}
 customElements.define('custom-forms', Forms);
 export default Forms;
